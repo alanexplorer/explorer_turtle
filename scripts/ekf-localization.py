5f3548684b625cb138e0d7bcd571f6df826f1089
alanexplorer <contatosalan@outlook.com>
2020-03-21 11:31:24 -0300

    add function from ArUco to Rviz

--- scripts/ekf-localization.py
+++ scripts/ekf-localization.py
@@ -1,109 +1,157 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import Odometry
-from tf.transformations import euler_from_quaternion
-import std_msgs.msg
-from explorer_turtle.msg import * 
+import cv_bridge
+import cv2
+from cv2 import aruco #pylint:disable=no-name-in-module
+from sensor_msgs.msg import Image, CameraInfo
+from explorer_turtle.msg import Marker, MarkerArray #pylint:disable=import-error
+from geometry_msgs.msg import Pose
 import numpy as np
-from matplotlib.patches import Ellipse
-import matplotlib.pyplot as plt
-
-predicted_state_est = 0
-predicted_covariance_est = 0
-state_trans_uncertainty_noise = 0
-measurement =	0
-ave_meas_dist = 0
-
-pub = rospy.Publisher('state_estimate',Config, queue_size =10)
-def get_data():
-
-    rospy.init_node('localization', anonymous=True)
-    # define ros param
-    laser = rospy.get_param("/laser_topic")
-    odom = rospy.get_param("/odometry_topic")
-
-    rospy.Subscriber(laser, LaserScan, kinect_scan_estimate)
-
-    rospy.Subscriber(odom, Odometry, odom_state_prediction)
-
-	# The callback is scheduled for every 1/1127 th of a second (8.8hz), should be ajusted
-
-    rospy.Timer(rospy.Duration(0.1127), meas_update_step, oneshot=False)
-
-    rospy.spin()
-    
-def kinect_scan_estimate(msg):
-    
-    global measurement
-    global ave_meas_dist
-
-    measurement = msg.ranges
-    
-    sum_dist = 0
-    length = 0
-	
-    for i in range (580, 600):
-        if str(measurement[i]) != 'nan' :
-            sum_dist += measurement[i]
-            length += 1
-            
-    if length != 0:
-        ave_meas_dist = sum_dist/length  
+from math import sin, cos, sqrt, atan2
+from tf.transformations import quaternion_from_euler
+import tf
+from marker_rviz import MarkerRviz
 
 
-def odom_state_prediction(msg):
+# Launch file needs to launch usb_cam_node: rosrun usb_cam usb_cam_node
+# To get around the error, first roslaunch usb_cam usb_cam-test.launch
+# Subscribe to /usb_cam/image_raw, which publishes sensor_msgs/Image
 
-    global predicted_state_est
-    global state_trans_uncertainty_noise
-    global predicted_covariance_est
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
 
-    (roll,pitch,yaw) = euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+ARUCO_SQUARE_SIZE = 0.1
 
-    x = msg.pose.pose.position.x
-    y = msg.pose.pose.position.y
+MX_MARKER = 20
 
-    predicted_state_est = Config(x,y,yaw)
+# The grid board type we're looking for
+board = aruco.GridBoard_create(
+        markersX = 1,
+        markersY = 1,
+        markerLength = ARUCO_SQUARE_SIZE, # In meters
+        markerSeparation = 0.1, # In meters
+        dictionary = ARUCO_DICT
+    )
 
-    state_trans_uncertainty_noise= np.array([[msg.pose.covariance[0],msg.pose.covariance[1],msg.pose.covariance[5]],[msg.pose.covariance[6],msg.pose.covariance[7],msg.pose.covariance[11]], [msg.pose.covariance[30],msg.pose.covariance[31],msg.pose.covariance[35]]])
+rvecs, tvecs = None, None
 
-    state_transition_jacobian = np.array([[1,0,0],[0,1,0],[0,0,1]])
+class EkfLocalization():
 
-    predicted_covariance_est = state_transition_jacobian*predicted_covariance_est*np.transpose(state_transition_jacobian)+state_trans_uncertainty_noise
+    def __init__(self):
 
-def meas_update_step(event):
+        #rospy.loginfo("Starting node")
 
-    global pub
-    global predicted_covariance_est
+        self.image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, callback=self.image_callback)
+        self.caminfo_sub = rospy.Subscriber('/usb_cam/camera_info', CameraInfo, callback=self.caminfo_callback)
+        self.marker_image_pub = rospy.Publisher('/explorer/marker/image', Image, queue_size=10)
+        self.marker_pub = rospy.Publisher('/explorer/marker/position', Pose, queue_size=10)
+        self.marker_array_pub = rospy.Publisher('/explorer/marker/array', MarkerArray, queue_size=10)
 
-    expected_meas = np.cross(np.array([0, 1, 0]), np.array([predicted_state_est.x, predicted_state_est.y, predicted_state_est.th]))
+        self.bridge = cv_bridge.CvBridge()
 
-    meas_residual = ave_meas_dist - expected_meas
+        self.font = cv2.FONT_HERSHEY_PLAIN
 
-    meas_noise_covariance = 0.005
+        self.CAMERA_MATRIX = None
+        self.DISTORTION_COEFFICIENTS = None
+        self.have_cam_info = False
 
-    H = np.array([[9999, 0 , 0],[0, 1, 0],[0 , 0, 9999]])
+        self.markerList = []
 
-    residual_covariance = H*predicted_covariance_est*np.transpose(H)+meas_noise_covariance
+        for i in range(MX_MARKER):
+            self.markerList.append(False)
 
-    kalman_gain = predicted_covariance_est*np.transpose(H)*np.linalg.inv(residual_covariance)
+        # list with rooms parameters 
 
-    updated_state_estimate =  np.array([predicted_state_est.x, predicted_state_est.y, predicted_state_est.th]) + np.dot(kalman_gain, meas_residual)
+        self.room_0 = MarkerRviz("sl0", 0, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.5, 0.05, 1.0), (0.0, 1.0, 0.0))
+        self.room_1 = MarkerRviz("sl1", 0, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.5, 0.05, 1.0), (0.0, 1.0, 0.0)) 
+        self.room_2 = MarkerRviz("sl2", 0, (0.0, 0.0, 0.0), (0.0, 0.0, 0.0), (0.5, 0.05, 1.0), (0.0, 1.0, 0.0)) 
 
-    predicted_covariance_est= (np.identity(3) - np.cross(kalman_gain,H))*predicted_covariance_est
+        self.rate = rospy.Rate(10) # 10hz
 
-    state_estimate = Config(updated_state_estimate[0], updated_state_estimate[1], updated_state_estimate[2])
+    def caminfo_callback(self, caminfo_msg):
 
-    rospy.logdebug(state_estimate)
+        if self.have_cam_info:
+            pass
+        else:
+            if caminfo_msg.K == [0, 0, 0, 0, 0, 0, 0, 0, 0]:
+                rospy.logwarn("CameraInfo message has K matrix all zeros")
+            else:
+                self.DISTORTION_COEFFICIENTS = caminfo_msg.D
+                self.CAMERA_MATRIX = np.zeros((3, 3))
 
-    print(state_estimate.x, state_estimate.y, state_estimate.th)
+                for i in range(0, 3):
+                    for j in range(0, 3):
+                        self.CAMERA_MATRIX[i][j] = caminfo_msg.K[i * 3 + j]
 
-    pub.publish(state_estimate)
+                self.have_cam_info = True
+
+
+    def image_callback(self, img_msg):
+
+        if self.have_cam_info:
+
+            markerList = MarkerArray()
+            markerList.header = img_msg.header
+
+            try:
+                cv_img = self.bridge.imgmsg_to_cv2(img_msg, "bgr8")
+
+            except cv_bridge.CvBridgeError as e:
+                rospy.loginfo(e)
+
+            gray_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2GRAY)
+
+            corners, ids, _ = aruco.detectMarkers(gray_img, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+
+            rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, ARUCO_SQUARE_SIZE, self.CAMERA_MATRIX, self.DISTORTION_COEFFICIENTS)
+
+            test_img = cv_img
+
+            if len(corners) > 0:
+                for i in range(ids.size):
+
+                    self.markerStatusList(ids[i, 0])
+                    test_img = aruco.drawDetectedMarkers(test_img, corners, ids)
+                    test_img = aruco.drawAxis(test_img, self.CAMERA_MATRIX, self.DISTORTION_COEFFICIENTS, rvecs[i], tvecs[i], ARUCO_SQUARE_SIZE)
+               
+            rosified_test_img = self.bridge.cv2_to_imgmsg(test_img, encoding="bgr8")
+            self.marker_image_pub.publish(rosified_test_img)
+
+    def markerStatusList(self, i):
+
+        if i <= MX_MARKER:
+            self.markerList[i] = True
+
+    def markerVisualization(self):
+
+        if self.markerList[6] == True:
+            self.room_0.publish()
+        if self.markerList[8] == True:
+            self.room_1.publish()
+        if self.markerList[2] == True:
+            self.room_2.publish()
+
+        br_room_0 = tf.TransformBroadcaster()
+        br_room_0.sendTransform((0.0, 0.0, 0.5), quaternion_from_euler(0.0, 0.0, 0.0), rospy.Time.now(), "sl0", "map")
+
+        br_room_1 = tf.TransformBroadcaster()
+        br_room_1.sendTransform((1.0, 0.0, 0.5), quaternion_from_euler(0.0, 0.0, 0.0), rospy.Time.now(), "sl1", "map")
+
+        br_room_2 = tf.TransformBroadcaster()
+        br_room_2.sendTransform((1.0, -2.0, 0.5), quaternion_from_euler(0.0, 0.0, 0.0), rospy.Time.now(), "sl2", "map")
+
+    def run(self):
+        while not rospy.is_shutdown():
+
+            self.markerVisualization()
+            self.rate.sleep()
 
 if __name__ == '__main__':
+    rospy.init_node('aruco_node')
+
     try:
-        get_data()
-        
+        e = EkfLocalization()
+        e.run()
     except rospy.ROSInterruptException:
         pass
