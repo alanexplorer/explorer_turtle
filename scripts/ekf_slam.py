6e4dc0ba71d2cd027f1ce9e729f24504730bc23e
alanexplorer <contatosalan@outlook.com>
2020-04-28 16:40:22 -0300

    Simulation ok

--- scripts/ekf_slam.py
+++ scripts/ekf_slam.py
@@ -27,28 +27,7 @@ sigma_range = 5.8e-15
 sigma_bearing = 0.5
 sigma_id = 1e16
 
-fig, ax = plt.subplots()
-x_l = [0]*NUMBER_MARKERS
-y_l = [0]*NUMBER_MARKERS
-types = ['0', '1', '2', '3', '4', '5', '6', '7']
-robot_odom, = ax.plot([0], [0], color='black', marker='o', markersize=12)
-robot_estimate, = ax.plot([0], [0], color='red', marker='*', markersize=12)
-marker_aruco, = ax.plot(x_l, y_l, color='lime', marker='s', markersize=10, linestyle="")
-plt.ion()
-plt.xlim(-10, 20)
-plt.ylim(-20, 10)
-plt.xlabel('X', fontsize=10)  # X axis label
-plt.ylabel('Y', fontsize=10)  # Y axis label
-plt.title('ekf localization')
-#plt.legend()
-plt.grid(True)  # Enabling gridding
-
-
 class EkfLocalization:
-
-    # motion_mode x, y psi 
-    # control - control robot velocity linear and angular
-    # Jr - Jacobian of measurement model with respect to robot pose
 
     def __init__(self):
 
@@ -62,22 +41,15 @@ class EkfLocalization:
 
         # important variables
         self.control = np.array([0, 0], dtype='float64').transpose()
-        self.motion_model = np.array([0, 0, 0], dtype='float64').transpose()
         self.last_odom = np.array([0, 0, 0], dtype='float64').transpose()
         self.odom_init = False
-        self.markers_info = None
+        self.measurement = None
         self.markerStatus = False
 
         # Choose very small process covariance because we are using the ground truth data for initial location
         self.sigma = np.diagflat([1e-10, 1e-10, 1e-10])
         self.H = 0
         self.Q = np.diagflat(np.array([sigma_range, sigma_bearing, sigma_id])) ** 2
-
-        self.x_path_odom = np.array([0], dtype='float64')
-        self.y_path_odom = np.array([0], dtype='float64')
-
-        self.x_path_ekf = np.array([0], dtype='float64')
-        self.y_path_ekf = np.array([0], dtype='float64')
 
         # Init Subscriber
         rospy.Subscriber('odom', Odometry, self.odom_callback)
@@ -94,22 +66,16 @@ class EkfLocalization:
             if self.odom_init and self.markerStatus:
                 pos = self.last_odom
                 control = self.control
-                measurements = self.markers_info
-                i = measurements[2]
+                Z = self.measurement
+                i = Z[2] # Feature
                 self.prediction(pos, control)
-                self.update()
-
-                Z  = measurements
+                self.sigma = self.G.dot(self.sigma).dot(self.G.T) + self.R
                 ZHAT = self.aruco_true(self.ARUCO[i], i, pos)
-                print("measurements", Z)
-                print("expected", ZHAT)
                 residual = np.subtract(Z, ZHAT)
-                
                 S = self.H.dot(self.sigma).dot(self.H.T) + self.Q
                 K = self.sigma.dot(self.H.T).dot(np.linalg.inv(S))
                 mu = pos + np.dot(K, residual)
                 self.sigma = (np.identity(3) - np.cross(K, self.H))*self.sigma
-
                 self.pub_position(mu, self.sigma)
                 self.resetMarkers()
 
@@ -117,15 +83,10 @@ class EkfLocalization:
 
     def resetMarkers(self):
 
-        self.markers_info = None
+        self.measurement = None
         self.markerStatus = False
 
     def pub_position(self, mu, sigma):
-
-        self.x_path_ekf = np.insert(self.x_path_ekf, 0, mu[0])
-        self.y_path_ekf = np.insert(self.y_path_ekf, 0, mu[1])
-
-        self.drawing_plot(mu)
         
         # since all odometry is 6DOF we'll need a quaternion created from yaw
         quat = quaternion_from_euler(0, 0, mu[2])
@@ -137,7 +98,6 @@ class EkfLocalization:
 
         # publish the message
         self.estimate_pub.publish(estimate)
-
 
     def prediction(self, pos, control):
 
@@ -156,12 +116,6 @@ class EkfLocalization:
         self.G[0][2] = G_02
         self.G[1][2] = G_12
 
-    def update(self):
-
-        #Calculated the total uncertainty of the predicted state estimate
-
-        self.sigma = self.G.dot(self.sigma).dot(self.G.T) + self.R
-
     def odom_callback(self, msg):
 
         # get x, y and yaw
@@ -176,37 +130,26 @@ class EkfLocalization:
 
         if not self.odom_init:
             self.last_odom = np.array([x, y, yaw], dtype='float64').transpose()
+            self.odom_init = True
 
         # Get control vector (change in linear displacement and rotation)to estimate current pose of the robot
-
-        distance = calcDistance(current_odom, self.last_odom)
-        theta = angdiff(current_odom[2], self.last_odom[2])
-
         self.last_odom = current_odom
-
-        self.motion_model = np.array([distance, theta], dtype='float64').transpose()
-
         v = msg.twist.twist.linear.x
         w = msg.twist.twist.angular.z
         self.control = np.array([v, w], dtype='float64').transpose()
-
-        self.x_path_odom = np.insert(self.x_path_odom, 0, x)
-        self.y_path_odom = np.insert(self.y_path_odom, 0, y)
 
         # UNCERTAINTY INTRODUCED BY STATE TRANSITION (MEAN = 0, COVARIANCE PUBLISHED BY ODOM TOPIC: )
         # Odom covariance matrix is 6 x 6. We need a 3 x 3 covariance matrix of x, y and theta. Omit z, roll and pitch data. 
         self.R = np.array([[msg.pose.covariance[0],msg.pose.covariance[1],msg.pose.covariance[5]],
         [msg.pose.covariance[6],msg.pose.covariance[7],msg.pose.covariance[11]], [msg.pose.covariance[30],msg.pose.covariance[31],msg.pose.covariance[35]]])
 
-        self.odom_init = True
-	
     def marker_callback(self, msg):
 
         try:
             index = msg.id
             dist = msg.distance
             th = msg.angle
-            self.markers_info = np.array([dist, th , index]).transpose()
+            self.measurement = np.array([dist, th , index]).transpose()
             self.markerStatus = True
         except:
             pass
@@ -231,34 +174,6 @@ class EkfLocalization:
         Z = np.array([dist, th, index]).transpose()
 
         return Z
-
-    def drawing_plot(self, mu):
-	
-        plt.show(block=False)
-
-        robot_odom.set_xdata(self.last_odom[0])
-        robot_odom.set_ydata(self.last_odom[1])
-        ax.draw_artist(ax.patch)
-        ax.draw_artist(robot_odom)
-
-        robot_estimate.set_xdata(mu[0])
-        robot_estimate.set_ydata(mu[1])
-        ax.draw_artist(ax.patch)
-        ax.draw_artist(robot_estimate)
-
-        cnt = 0
-        for i in self.ARUCO:
-            x_l[cnt] = self.ARUCO[i][0]
-            y_l[cnt] = self.ARUCO[i][1]
-            plt.text(x_l[cnt]-0.2, y_l[cnt]-0.3, types[cnt], fontsize=9)
-            cnt += 1
-
-        marker_aruco.set_xdata(x_l)
-        marker_aruco.set_ydata(y_l)
-        ax.draw_artist(ax.patch)
-        ax.draw_artist(marker_aruco)
-
-        fig.canvas.flush_events()
 
 if __name__ == '__main__':
     try:
